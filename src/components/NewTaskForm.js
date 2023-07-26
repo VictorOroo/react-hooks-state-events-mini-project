49387e86c7114996885a8e3538113a09ee5daa8f
victoronyonkaoroo <victor.oroo@student.moringaschool.com>
2023-07-26 19:17:55 +0300

    done with lab

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,8 +1,20 @@
 import React from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const text = form.text.value;
+    const category = form.category.value;
+
+    if (text && category) {
+      onTaskFormSubmit({ text, category });
+      form.reset();
+    }
+  };
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
         <input type="text" name="text" />
@@ -10,7 +22,11 @@ function NewTaskForm() {
       <label>
         Category
         <select name="category">
-          {/* render <option> elements for each category here */}
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
